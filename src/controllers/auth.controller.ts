654f36109f68e4efde03c6d7b10416567283551f
Ahmad Wahidurrijal <ahmad.wahidurrijal@binus.com>
2025-05-25 14:59:53 +0700

    commit kedua

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as Yup from 'yup';
 import UserModel from '../models/user.model';
+import { encrypt } from '../utils/encryption';
 
 type Tregister ={
     fullName: string;
@@ -10,20 +11,22 @@ type Tregister ={
     confirmPassword: string;
 };
 
+type Tlogin ={
+    identifier: string;
+    password: string;
+}
+
+
 const registerValidateSchema = Yup.object({
     fullName: Yup.string()
         .required('Fullname is required'),
     username: Yup.string()
-        .required('Username is required')
-        .min(3, 'Username must be at least 3 characters long')
-        .max(20, 'Username must be at most 20 characters long'),
+        .required('Username is required'),
     email: Yup.string()
         .required('Email is required')
         .email('Email is not valid'),
     password: Yup.string()
-        .required('Password is required')
-        .min(8, 'Password must be at least 8 characters long')
-        .max(20, 'Password must be at most 20 characters long'),
+        .required('Password is required'),
     confirmPassword: Yup.string()
         .required('Confirm Password is required')
         
@@ -69,6 +72,49 @@ export default {
                 data: null
             })
         }
+        },
+    async login(req:Request, res:Response){
+        const {
+            identifier,
+            password
+        } = req.body as unknown as Tlogin;
+        try {
+            //amibl data user berdasarkan identifier -> email dan username
+            //validasi password 
+            const userByIdentifier = await UserModel.findOne({
+                $or: [
+                    { email: identifier },
+                    { username: identifier }
+                ],
+            })
+
+            if (!userByIdentifier) {
+                return res.status(403).json({
+                    message: 'User not found',
+                    data: null
+                })
+            }
+            //validasi password
+            const validatePassword: boolean = encrypt(password) === userByIdentifier.password;
+
+            if (!validatePassword){
+                return res.status(403).json({
+                    message: 'Invalid password',
+                    data: null
+                })
+            }
+            //jika validasi password benar, maka user bisa login
+            res.status(200).json({
+                message: 'Login Berhasil',
+                data: userByIdentifier,
+            })
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message : err.message,
+                data: null
+            })
         }
+    }
     }
     
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import authController from '../controllers/auth.controller';
 
 const router = express.Router();
+const asyncHandler = (fn: any) => (req: express.Request, res: express.Response, next: express.NextFunction) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
 router.post('/auth/register', authController.register);
+router.post('/auth/login', asyncHandler(authController.login));
 
 export default router;
